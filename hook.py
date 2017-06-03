036626f77d1611b8bd4f4df8ab5375c7a0b30ae2
RohanArora13 <RohanArora1313@gmail.com>
2017-06-03 16:23:01 +0530

    Some minor bug fixed

--- hook.py
+++ hook.py
@@ -1,5 +1,5 @@
 import urllib2
-import time,os,shutil,inspect,re,sys
+import time,os,shutil,re,sys
 import zipfile
 
 #the intro and info
@@ -34,7 +34,7 @@ ________________________________________________________________________________
 # very silly method but works for me which is made by me
 
 try:
-    response = urllib2.urlopen('url of site with info about download ')
+    response = urllib2.urlopen('http://freetexthost.com/gxgc5ymqh4')
     txt = response.read()
     t1=re.findall(r'<div id="contentsinner">(.*?)<div style="clear: both;"><!-- --></div>',txt,re.DOTALL)
     t1=t1[0]
@@ -45,15 +45,21 @@ try:
     SH=t1[1].strip()
     SHN=t1[2].strip()
     ST=t1[3].strip()
+    try:
+        url2=t1[4].strip()
+    except:
+        url2=None
 except:
     print "cant reach download server ... Try to close firewall or check internet connection..."
     time.sleep(5)
     quit()
 
+
 #print the version of script hooks
 print"Script Hook V "+str(SH)+"             "+"Script Hook V .NET "+str(SHN)+"             "+" Simple Trainer "+str(ST)
 
-print"________________________________________________________________________________________________________"
+
+print"________________________________________________________________________________________________________\n"
 
 
 # get file path
@@ -91,6 +97,7 @@ ask()
 
 
 print "Downloading Zip Files..... \n"
+# method 1 for downloading
 try:
     t1=time.time()
     f = urllib2.urlopen(url)
@@ -104,15 +111,52 @@ except:
     time.sleep(5)
     quit()
 
+
+if(os.path.getsize("data.zip")>=2000000):
+    pass
+else:
+    if(url2!=None):
+        url=url2
+    else:
+        pass
+
+
 print "Extracting Files.... \n"
 
+def D2():  # method 2 for downloading..
+    print "Downloading Zip Files again..... \n"
+    try:
+        t1=time.time()
+        f = urllib2.urlopen(url)
+        data = f.read()
+        with open(rar, "wb") as code:
+           code.write(data)
+        t2=time.time()
+        print "Time Taken ="+ str(t2-t1)+" sec\n"
+    except:
+        print ""
+    try:
+        t1=time.time()
+        f = urllib2.urlopen(url)
+        data = f.read()
+        with open(rar, "wb") as code:
+           code.write(data)
+        t2=time.time()
+        print "Time Taken ="+ str(t2-t1)+" sec\n"
+    except:
+        print "can't reach server ... Try to close firewall or check internet connection..."
+        time.sleep(5)
+        quit()
 
 # unzipping
-
-zip_ref = zipfile.ZipFile(me+"\\"+rar, 'r')
-zip_ref.extractall(me+"\data")
-zip_ref.close()
-
+try:
+    zip_ref = zipfile.ZipFile(me+"\\"+rar, 'r')
+    zip_ref.extractall(me+"\data")
+    zip_ref.close()
+except:
+    print"Zip File not downloaded properly!"
+    print"Trying To download again \n"
+    D2()
 
 print "Installing... \n"
 
@@ -127,7 +171,7 @@ def copy():
             shutil.copy(full_file_name,dest)
 
 copy()
-print "Verifying Installtion.... \n"
+print "Verifying Installation.... \n"
 
 def find(name, path):
     for root, dirs, files in os.walk(path):
